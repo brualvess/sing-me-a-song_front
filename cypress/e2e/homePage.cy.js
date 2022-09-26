b48629bc194726bcc82062ba4737a5e8973edfcb
brualvess <oliveirabrunna2210@gmail.com>
2022-09-26 11:20:43 -0300

    test e2e  score -5

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -4,13 +4,13 @@ describe('Testa rota /', () => {
 
     cy.visit(BASE_URL)
 
-    cy.get('#name').type('teste')
+    cy.get('#name').type('meditate')
     cy.get('#youtubeLink').type('https://www.youtube.com/watch?v=Uv9URjIBtUQ')
     cy.get('.create-recommendation').click();
   
     cy.url().should('equal', BASE_URL)
   })
-  
+
   it('Votar em uma recomendação', ()=>{
     for (let i = 0; i <= 5; i++) {
       cy.get("#upvote").click();
@@ -18,9 +18,28 @@ describe('Testa rota /', () => {
   })
 
   it('Votar negativo em uma recomendação', ()=>{
-    for (let i = 0; i <= 1; i++) {
+    for (let i = 0; i <= 6; i++) {
       cy.get("#downvote").click();
     }
   })
 
+})
+describe('Testa rota /', () => {
+  it('Excluir recomendação com pontuação -5', ()=>{
+
+    cy.visit(BASE_URL)
+
+    cy.get('#name').type('meditação')
+    cy.get('#youtubeLink').type('https://www.youtube.com/watch?v=Uv9URjIBtUQ')
+    cy.get('.create-recommendation').click();
+  
+    for (let i = 0; i <= 5; i++) {
+      cy.get("#upvote").click();
+    }
+
+    for (let i = 0; i <= 11; i++) {
+      cy.get("#downvote").click();
+    }
+
+  })
 })
