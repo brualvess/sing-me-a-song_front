5afbbfb15f3b9fd132350c5ac18871a4ae1035e3
brualvess <oliveirabrunna2210@gmail.com>
2022-09-26 11:04:12 -0300

    fix

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -4,20 +4,23 @@ describe('Testa rota /', () => {
 
     cy.visit(BASE_URL)
 
-    cy.get('#name').type('Test')
+    cy.get('#name').type('teste')
     cy.get('#youtubeLink').type('https://www.youtube.com/watch?v=Uv9URjIBtUQ')
     cy.get('.create-recommendation').click();
   
     cy.url().should('equal', BASE_URL)
   })
+  
   it('Votar em uma recomendação', ()=>{
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i <= 5; i++) {
       cy.get("#upvote").click();
     }
   })
+
   it('Votar negativo em uma recomendação', ()=>{
     for (let i = 0; i <= 1; i++) {
       cy.get("#downvote").click();
     }
   })
+
 })
